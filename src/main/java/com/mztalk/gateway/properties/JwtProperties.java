287b7a7fcfb7c8c0d4a0ceaafc4de68283bae168
bmm522 <rlawldls11@naver.com>
2022-12-28 19:47:18 +0900

    feat : docker test start

---
+++ src/main/java/com/mztalk/gateway/properties/JwtProperties.java
@@ -0,0 +1,7 @@
+package com.mztalk.gateway.properties;
+
+public interface JwtProperties {
+    String SECRET = "secret1234Mztalk7890dfjsjfjRtgf523dsFSADdffdfdfdss";
+    String JWT_PREFIX = "Bearer ";
+    String REFRESHTOKEN_PREFIX = "RefreshToken ";
+}
